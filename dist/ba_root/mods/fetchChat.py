0ac6db838f4e9ce774eef5ce1e056cb9cf984a07
root <root@BOOK-S4QLCUENU3.>
2024-12-11 18:45:04 +0000

    ytf _hooks.py resets after running the script

--- dist/ba_root/mods/fetchChat.py
+++ dist/ba_root/mods/fetchChat.py
@@ -1,10 +1,11 @@
 import bascenev1 as bs
+import hello
 
 def filter_chat_message(msg: str, client_id: int):
 
     try:
         if msg == "hello":
-            hello.hello(msg)
+            hello.hello()
             return None
     except:
         pass
--- dist/ba_root/mods/hello.py
+++ dist/ba_root/mods/hello.py
@@ -1,8 +1,7 @@
 import bascenev1 as bs
 
-def hello(msg):
-    if msg == "hello":
-        bs.broadcastmessage(
-            "Hello World",
-            color = (0, 1, 0.5)
-            )
+def hello():
+    bs.broadcastmessage(
+        "Hello World",
+        color = (0, 1, 0.5)
+        )
