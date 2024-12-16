67c84f835e973cc2daca794f5cfe9f14d6cc92b6
HeyFang <heyfang36@yahoo.com>
2024-12-16 09:46:33 +0000

    fixed kick cmd

--- dist/ba_root/mods/fetchChat.py
+++ dist/ba_root/mods/fetchChat.py
@@ -1,23 +1,34 @@
-# ba_meta require api 9
-import hello
 import babase as ba
 import bascenev1 as bs
+import json
+import os
+import commands as cmd
 
-print("fetchChat is called")
+def filter_chat_message(msg: str, client_id: int) -> str:
+    if not msg.startswith("/"):
+        return msg
 
-def filter_chat_message(msg: str, client_id: int) -> str | None:
-    print(msg, client_id)
-    
-    match msg:
-        
-        case "hello":
-            hello.hello()
+    args = msg.split()
+    command = args[0].lstrip("/")
 
-        case "/end":
-            hello.end(client_id)
+    if command not in {"kick", "hi"}:
+        bs.broadcastmessage("No such command", transient=True, clients=[client_id], color=(1,0,0))
+        return msg
 
-        case _:
-            return msg
+    ros = bs.get_game_roster()
+    for entity in ros:
+        if entity["client_id"] == client_id:
+            pbid = entity["account_id"]
+            admin_path = os.path.join(os.getcwd(), "ba_root/mods/admin.json")
+            with open(admin_path, "r") as file:
+                admins = json.load(file)["admins"]
+
+            if pbid in admins:
+                try:
+                    getattr(cmd, command)(args[1:])
+                except AttributeError as e:
+                    print(f"Error: {e}")
+            else:
+                print(f"{entity['players'][0]['name']} is not an admin")
 
     return msg
-
