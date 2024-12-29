9413a795c01e47b5937bb161ba53085ea3bf3c5a
Yuzuru <71698842+ItzYuzuruYT@users.noreply.github.com>
2024-12-29 23:18:21 +0530

    Update handlers.py

--- dist/ba_root/mods/handlers.py
+++ dist/ba_root/mods/handlers.py
@@ -1,7 +1,10 @@
 import bascenev1 as bs
 import bascenev1lib as bsl
+import babase as ba
 
 def on_game_begins(self):
+    # chars = ba.SpecialChar(64)
+    
     with bs.get_foreground_host_activity().context:
         self.header1 = bs.newnode(
             'text',
@@ -15,7 +18,7 @@ def on_game_begins(self):
                 # 'vr_depth': 390,
                 'scale': 1.4,
                 'color': (0.4, 0.8, 1.0, 1.0),
-                'text': f"\ue04d || EONI vs CYCLONES || {chr(0x1F600)}",
+                'text': f"{ba.charstr(ba.SpecialChar.CROWN)} || EONI vs CYCLONES || {ba.charstr(ba.SpecialChar.CROWN)}",
                 # 'transition': 'fade_in'
             },
         )
