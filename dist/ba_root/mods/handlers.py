d9cd67e6f206aba16949af943a2240cb004d01db
Yuzuru <71698842+ItzYuzuruYT@users.noreply.github.com>
2024-12-31 16:30:29 +0530

    Update handlers.py

--- dist/ba_root/mods/handlers.py
+++ dist/ba_root/mods/handlers.py
@@ -1,6 +1,7 @@
 import bascenev1 as bs
 import bascenev1lib as bsl
 import babase as ba
+import random
 
 def on_game_begins(self):
     # chars = ba.SpecialChar(64)
@@ -44,7 +45,7 @@ def on_game_begins(self):
         self.script2 = bs.newnode(
             'text',
             attrs={
-                'position': (20, 75),
+                'position': (25, 75),
                 'h_attach': 'left',
                 'h_align': 'left',
                 'v_attach': 'bottom',
@@ -61,7 +62,7 @@ def on_game_begins(self):
         self.script2 = bs.newnode(
             'text',
             attrs={
-                'position': (20, 50),
+                'position': (25, 50),
                 'h_attach': 'left',
                 'h_align': 'left',
                 'v_attach': 'bottom',
@@ -76,4 +77,44 @@ def on_game_begins(self):
         )
         
         
-        print(dir(self.script2))
+        self.loop_text = bs.newnode(
+            "text",
+            attrs = {
+                "position": (0, -200),
+                "h_attach": "center",
+                "h_align": "center",
+                "v_attach": "center",
+                "v_align": "center",
+                # "maxwidth": 300,
+                "shadow": 0.5,
+                "scale": 1.0,
+                "color": (0.4, 1.0, 0.8, 1),
+                "text": ""
+            }
+        )
+        
+        def change_text():
+            arr = [
+                "Join the official discord community by clicking on stats button",
+                "For ban related appeals contact the admins in discord server",
+                "Come play minecraft with us in the cyclones discord community"
+            ]
+            key = round(random.random() * (len(arr) - 1))
+            
+            self.loop_text.text = arr[key]
+            bs.timer(4, ba.Call( (lambda: self.loop_text.__setattr__("text", "")) ))
+        
+        bs.timer(6, ba.Call(change_text), repeat=True)
+        
+        
+        
+        bs.animate_array(node=self.loop_text, attr="color", size=3, 
+                   keys= {
+                        0.2: (0.4, 1.0, 0.8),
+                        0.4: (0.4, 0.8, 1.0),
+                        0.6: (0.4, 0.6, 0.8),
+                        0.8: (0.4, 0.4, 0.6),
+                        1.0: (0.4, 0.6, 0.4),
+                        1.2: (0.4, 0.8, 0.6),
+                        1.4: (0.4, 1.0, 0.8)
+                   }, loop=True)
