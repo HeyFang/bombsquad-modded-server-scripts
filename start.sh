e419a80e07f1253846ff5d3569009ebf433247a8
HeyFang <heyfang36@yahoo.com>
2025-01-04 17:00:06 +0000

    added setup.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,5 @@ sudo apt install python3-tinydb -y
 sudo apt update -y
 git clone https://github.com/HeyFang/bombsquad-modded-server-scripts.git
 cd bombsquad-modded-server-scripts
-tmux new -s 43210
 chmod +x ballisticakit_server
 chmod +x dist/ballisticakit_headless
-./ballisticakit_server
